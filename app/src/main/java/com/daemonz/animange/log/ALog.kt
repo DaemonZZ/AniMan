13925c1eebb6e8d0aefb829ba4279bce47413e5b
Thang <sgooophx@gmail.com>
2024-06-10 19:52:06 +0700

    Log rule

--- app/src/main/java/com/daemonz/animange/log/ALog.kt
+++ app/src/main/java/com/daemonz/animange/log/ALog.kt
@@ -7,12 +7,14 @@ class ALog {
     companion object {
         private const val TAG = BuildConfig.TAG
         fun d(tag: String, message: String) {
-            val maxLogSize = 3000
-            for (i in 0..message.length / maxLogSize) {
-                val start = i * maxLogSize
-                var end = (i + 1) * maxLogSize
-                end = if (end > message.length) message.length else end
-                Log.d("$TAG$tag", message.substring(start, end))
+            if (BuildConfig.BUILD_TYPE == "debug") {
+                val maxLogSize = 3000
+                for (i in 0..message.length / maxLogSize) {
+                    val start = i * maxLogSize
+                    var end = (i + 1) * maxLogSize
+                    end = if (end > message.length) message.length else end
+                    Log.d("$TAG$tag", message.substring(start, end))
+                }
             }
         }
 
@@ -21,11 +23,15 @@ class ALog {
         }
 
         fun i(tag: String, message: String) {
-            Log.i("$TAG$tag", message)
+            if (BuildConfig.BUILD_TYPE == "debug") {
+                Log.i("$TAG$tag", message)
+            }
         }
 
         fun w(tag: String, message: String) {
-            Log.w("$TAG$tag", message)
+            if (BuildConfig.BUILD_TYPE == "debug") {
+                Log.w("$TAG$tag", message)
+            }
         }
 
         fun v(tag: String, message: String) {
