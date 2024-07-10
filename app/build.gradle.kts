06166b1cb8a7ebe2e9206818891e968f22e5c816
thangdn6 <sgooophx@gmail.com>
2024-07-10 16:45:08 +0700

    Release v 1.3.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 }
 val versionMajor = 1
 val versionMinor = 3
-val versionPatch = 6
+val versionPatch = 7
 val versionClassifier = null
 val isSnapshot = true
 val minimumSdkVersion = 31
