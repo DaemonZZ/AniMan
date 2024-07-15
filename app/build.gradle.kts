c0e9fcf1ff029199b8b408afbc166d384f186dd6
Thang <sgooophx@gmail.com>
2024-07-15 22:13:41 +0700

    Release v1.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 }
 val versionMajor = 1
 val versionMinor = 4
-val versionPatch = 1
+val versionPatch = 2
 val versionClassifier = null
 val isSnapshot = true
 val minimumSdkVersion = 31
