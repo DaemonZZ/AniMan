0dcf5845d0b98e151e41d538ae4fad0e770bd381
Thang <sgooophx@gmail.com>
2024-08-24 08:09:22 +0700

    Release 1.5.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 }
 val versionMajor = 1
 val versionMinor = 5
-val versionPatch = 7
+val versionPatch = 8
 val versionClassifier = null
 val isSnapshot = true
 val minimumSdkVersion = 31
