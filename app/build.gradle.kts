f174d47d8633dac9bdee7b7981352f8093031dc1
Thang <sgooophx@gmail.com>
2024-08-31 22:29:48 +0700

    scale setting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 }
 val versionMajor = 1
 val versionMinor = 5
-val versionPatch = 11
+val versionPatch = 12
 val versionClassifier = null
 val isSnapshot = true
 val minimumSdkVersion = 31
