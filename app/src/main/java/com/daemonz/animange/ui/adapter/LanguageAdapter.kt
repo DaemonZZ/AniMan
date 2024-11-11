f691e1040344b3ae33414a88af0c5c776b15a536
Thang <sgooophx@gmail.com>
2024-11-11 21:27:22 +0700

    language ui

--- app/src/main/java/com/daemonz/animange/ui/adapter/LanguageAdapter.kt
+++ app/src/main/java/com/daemonz/animange/ui/adapter/LanguageAdapter.kt
@@ -1,7 +1,9 @@
 package com.daemonz.animange.ui.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.daemonz.animange.R
 import com.daemonz.animange.base.BaseRecyclerAdapter
 import com.daemonz.animange.base.OnItemClickListener
 import com.daemonz.animange.databinding.ItemLanguageBinding
@@ -14,7 +16,23 @@ class LanguageAdapter(onItemClickListener: OnItemClickListener<Language>) :
 
     override fun bindView(binding: ItemLanguageBinding, item: Language, position: Int) {
         binding.apply {
-            title.text = item.name
+            title.text = getLanguageName(item, root.context)
+            icon.setImageResource(getLangIcons(item))
         }
     }
+
+    private fun getLanguageName(item: Language, context: Context): String {
+        return when (item.id) {
+            "en" -> context.getString(R.string.english)
+            "vi" -> context.getString(R.string.vietnamese)
+            else -> "Unknown"
+        }
+    }
+
+    private fun getLangIcons(item: Language) = when (item.id) {
+        "en" -> R.drawable.ic_lang_us
+        "vi" -> R.drawable.ic_lang_vi
+        else -> R.drawable.ic_profile
+    }
+
 }
