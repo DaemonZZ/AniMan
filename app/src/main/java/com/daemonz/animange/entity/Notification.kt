49b712ef0bfa8eac03c94c80bcc3004ffae76d30
Thang <sgooophx@gmail.com>
2024-12-01 11:18:15 +0700

    update noti

--- app/src/main/java/com/daemonz/animange/entity/Notification.kt
+++ app/src/main/java/com/daemonz/animange/entity/Notification.kt
@@ -8,8 +8,16 @@ data class Notification(
     val id: String? = null,
     val title: String? = null,
     val message: String? = null,
-    val type: String? = null,
+    val type: NotificationType? = null,
     val time: String? = null,
     val img: String? = null,
-    val isRead: Boolean = false
+    val isRead: Boolean = false,
+    val isNew: Boolean = false,
+    val destination: String? = null
 ) : Serializable
+
+enum class NotificationType {
+    NEW_UPDATE,
+    REPLY_FEEDBACK,
+    NEWS
+}
