eea099a8a508ec269cc922529cfbf7dfd73007c6
Thang <sgooophx@gmail.com>
2025-03-26 18:04:47 +0700

    release 2.0.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 }
 val versionMajor = 2
 val versionMinor = 0
-val versionPatch = 12
+val versionPatch = 13
 val versionClassifier = null
 val isSnapshot = true
 val minimumSdkVersion = 31
